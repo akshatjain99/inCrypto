f91427e58c234821587c1e7e47800f0539b34ba0
Rishi110 <rishieric91@gmail.com>
2017-11-07 23:30:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/incrypto/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.android.incrypto;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView) findViewById(R.id.bitcoin_image);
+        img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // your code here
+                Intent intent = new Intent(MainActivity.this,PriceIndex.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/android/incrypto/PriceIndex.java
@@ -0,0 +1,17 @@
+package com.example.android.incrypto;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rishi Banerjee on 06-11-2017.
+ */
+
+public class PriceIndex extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.price_index);
+
+    }
+}
